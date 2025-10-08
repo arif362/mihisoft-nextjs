dee6117f5bb2c8d42bc3710fa919fdb4254b0790
Arif <gmarifulislamarif@gmail.com>
2025-10-08 09:47:19 +0600

    linting and formatting issues fixed

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -3,6 +3,7 @@
 import { motion } from 'framer-motion'
 import { FaFacebook, FaLinkedin, FaSkype, FaMapMarkerAlt, FaAward, FaUsers } from 'react-icons/fa'
 import { HiSparkles } from 'react-icons/hi'
+import Image from 'next/image';
 
 export default function TeamSection() {
   const teamMembers = [
@@ -98,9 +99,11 @@ export default function TeamSection() {
                 {/* Card Header with Image */}
                 <div className="relative flex-shrink-0">
                   <div className="h-80 overflow-hidden relative">
-                    <img 
+                    <Image 
                       src={member.image}
                       alt={member.name}
+                      width={150}
+                      height={150}
                       className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
                     />
                     {/* Gradient Overlay */}
